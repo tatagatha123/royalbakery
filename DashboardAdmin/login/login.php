e05df5fa3fcbbb59d8dcd891363a69b6086de466
Najmira Rahma Dina <EMAIL_GITHUB_KAMU>
2025-11-29 21:04:52 +0700

    revisi kode login

--- DashboardAdmin/login/login.php
+++ DashboardAdmin/login/login.php
@@ -39,7 +39,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INI COBA COBA - Royal Bakery</title>
+    <title>Royal Bakery</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
